109f54b2cb8cc47e0c4abad3d439d15f523a5af5
arthurmoulard <arthurmoulardperso@gmail.com>
2026-02-24 14:53:38 +0100

    Refactor Place model initialization to generate unique ID and initialize attributes

--- part2/app/models/places.py
+++ part2/app/models/places.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+import BaseModel
 import uuid
 from datetime import datetime
-import BaseModel
 
 
 class Place(BaseModel):
     def __init__(self, id, title, description, price, latitude, longitude, owner):
-        self.id = id
+        self.id = str(uuid.uuid4())
         self.title = title
         self.description = description
         self.price = price
         self.latitude = latitude
         self.longitude = longitude
         self.owner = owner
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.reviews = []
+        self.amenities = []
 
     @property
     def title(self):
